dcb5c4fe1b8553178d200a6497ef188251f4d8e8
mohammed-badsha <mohammedbadsha805@gmail.com>
2022-07-30 21:12:26 +0600

    methods overiteded

---
+++ methods_overi/src/methods_overi/A.java
@@ -0,0 +1,15 @@
+
+package methods_overi;
+
+
+public class A {
+    String Name = "Mohammed Badsha";
+    int Roll = 483017;
+    public void add(){
+        System.out.println(Name);
+        System.out.println(Roll);
+    }
+    
+public class B extends A
+    
+}
---
+++ methods_overi/src/methods_overi/Methods_overi.java
@@ -0,0 +1,14 @@
+
+package methods_overi;
+
+
+public class Methods_overi {
+    
+    
+            
+    public static void main(String[] args) {
+        
+         System.out.print("The time to do some hard work \n new line created or not ?");
+    }
+    
+}
