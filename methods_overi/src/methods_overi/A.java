69ac4aa518d55133b504a03cd1e084b268cefcad
mohammed-badsha <mohammedbadsha805@gmail.com>
2022-07-30 21:25:10 +0600

    method_overited

--- methods_overi/src/methods_overi/A.java
+++ methods_overi/src/methods_overi/A.java
@@ -3,13 +3,34 @@ package methods_overi;
 
 
 public class A {
-    String Name = "Mohammed Badsha";
-    int Roll = 483017;
+    String Name;
+    int Roll;
     public void add(){
         System.out.println(Name);
         System.out.println(Roll);
     }
     
-public class B extends A
-    
+public class B extends A{
+    String salary;
+    public void add(){
+    System.out.println(Name);
+    System.out.println(Roll);
+    System.out.println(salary);
+    }
 }
+public class Student{
+    public static void main(String[] args){
+    
+        B obj = new B();
+        obj.Name = "Mohammed Badsha";
+        obj.Roll = 483017;
+        obj.salary = 20,000;
+        
+        A obj2 = new A();
+        obj2.Name = "Mohammed hamim";
+        obj2.Roll = 169785;
+        
+    }
+}
+}
+
