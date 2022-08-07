236d2e82effef824d6f26d1d181b464e3885daf6
mohammed-badsha <mohammedbadsha805@gmail.com>
2022-08-07 20:32:37 +0600

    switch_with variable

--- switch/src/pkgswitch/Switch.java
+++ switch/src/pkgswitch/Switch.java
@@ -4,40 +4,38 @@ import java.util.Scanner;
 public class Switch {
 
     public static void main(String[] args) {
-       Scanner sr = new Scanner(System.in);
-       
-        int numbers;
-        System.out.print("Enter the value of Numbers :");
-        numbers =sr.nextInt();
-        String result;
+        Scanner in = new Scanner(System.in);
+        int number; String result;
+        System.out.print("Enter your point : ");
+        number =in.nextInt();
         
-        switch(numbers){
+        switch(number){
             
             case 80 :
-                System.out.println("Your CGPA IS  : A+");
+                result = "A+";
                 break;
                 
             case 70 :
-                System.out.println("Your CGPA IS : A");
-                break;
-            case 60 :
-               System.out.println("Your CGPA IS : A_");
-                break;
-            case 50 :
-                System.out.println("Your CGPA IS : B-");
-                break;
-            
-            case 40 :
-                System.out.println("Your CGPA IS : C");
-                break;
-            default:
-                System.out.println("Result is :Your are fall bro try Next time okey");
+                result = "A";
                 break;
                 
-            
-     
-          
-        
+            case 60 :
+                result = "A-";
+                break;
+            case 50 :
+                result = "B";
+                break;
+            case 40 :
+                result = "C";
+                break;
+            case 33 :
+                result = "C-";
+                break;
+            default:
+                result = "Fall";
+                break;                     
+        }
+        System.out.println("Your Result is : "+result);
         
     }
     
